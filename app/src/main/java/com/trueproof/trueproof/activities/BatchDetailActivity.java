5e1a0c8cf59f5213b12e9aaa69012dc6d32f2ec6
Ed <houedward@gmail.com>
2021-05-21 16:46:38 -0700

    Create Activity and layout file for each wireframe

--- app/src/main/java/com/trueproof/trueproof/activities/BatchDetailActivity.java
+++ app/src/main/java/com/trueproof/trueproof/activities/BatchDetailActivity.java
@@ -1,14 +1,16 @@
-package com.trueproof.trueproof;
+package com.trueproof.trueproof.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.trueproof.trueproof.R;
+
+public class BatchDetailActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_batch_detail);
     }
 }
