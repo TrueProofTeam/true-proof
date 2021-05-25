e57e6a43d90e4f9cf4785935667ed087f9a61def
Stephen Webber <offgridauthor@gmail.com>
2021-05-24 18:48:30 -0700

    new batch

--- app/src/main/java/com/trueproof/trueproof/activities/NewBatchActivity.java
+++ app/src/main/java/com/trueproof/trueproof/activities/NewBatchActivity.java
@@ -4,14 +4,22 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.amplifyframework.api.graphql.model.ModelMutation;
+import com.amplifyframework.core.Amplify;
 import com.amplifyframework.datastore.generated.model.Batch;
 import com.trueproof.trueproof.R;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class NewBatchActivity extends AppCompatActivity {
+
+    static String TAG = "t.newBatch";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,7 +32,14 @@ public class NewBatchActivity extends AppCompatActivity {
             String batchId = ((EditText) findViewById(R.id.editTextBatchIdNewBatch)).getText().toString();
             Batch batch = Batch.builder()
                     .batchIdentifier(batchId).batchNumber(batchNum).type(batchType).build();
+            Amplify.API.mutate(ModelMutation.create(batch),
+                    response ->{
+                        Log.i(TAG, "oncreate success");
+                    },
+                    response -> {
+                        Log.i(TAG, "onCreate: fail");
 
+                    });
 
             Toast.makeText(this, "Started batch " + batchId, Toast.LENGTH_LONG).show();
             startActivity(new Intent(this,TakeMeasurementActivity.class));
