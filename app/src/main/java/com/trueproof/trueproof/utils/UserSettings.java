3d05dfd06d79ad7d00b1157b7ece9f6a196baf60
Ed <houedward@gmail.com>
2021-05-25 16:03:54 -0700

    Repository/callback example code

--- app/src/main/java/com/trueproof/trueproof/utils/UserSettings.java
+++ app/src/main/java/com/trueproof/trueproof/utils/UserSettings.java
@@ -3,6 +3,19 @@ package com.trueproof.trueproof.utils;
 import android.content.Context;
 import android.content.SharedPreferences;
 
+import com.amplifyframework.api.ApiException;
+import com.amplifyframework.api.graphql.model.ModelMutation;
+import com.amplifyframework.api.graphql.model.ModelQuery;
+import com.amplifyframework.core.Amplify;
+import com.amplifyframework.core.Consumer;
+import com.amplifyframework.datastore.generated.model.Distillery;
+import com.amplifyframework.datastore.generated.model.TemperatureUnit;
+import com.amplifyframework.datastore.generated.model.User;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 import javax.inject.Inject;
 import javax.inject.Singleton;
 
@@ -11,19 +24,38 @@ import dagger.hilt.android.qualifiers.ApplicationContext;
 @Singleton
 public class UserSettings {
     final static String SHARED_PREFERENCES_NAME = "trueproof.trueproof";
+    static String EXAMPLE_EMAIL = "me@you.com";
+
     private SharedPreferences sharedPreferences;
 
     @Inject
-    UserSettings(@ApplicationContext Context context, UserRepository userRepository) {
+    UserSettings(@ApplicationContext Context context) {
         sharedPreferences = context.getSharedPreferences(SHARED_PREFERENCES_NAME, context.MODE_PRIVATE);
     }
 
-    public void setUserName() {
+    public String getEmail() {
+        return EXAMPLE_EMAIL;
     }
 
-    public void getUserName() {
+    public Distillery getDistillery() {
+        List<String> emails = new ArrayList<>();
+        emails.add(EXAMPLE_EMAIL);
+        return Distillery.builder()
+                .dspId("WA-EXAMPLE")
+                .name("Olde Tyme Whiskey Distillery")
+                .users(emails)
+                .build();
     }
 
-    public void getTemperature() {
+    public void getDistillery(Consumer<Distillery> success, Consumer<Exception> fail) {
+        // TODO
+    }
+
+    public Double getDefaultTemperatureCorrection() {
+        return 0.5;
+    }
+
+    public Double getDefaultHydrometerCorrection() {
+        return -0.2;
     }
 }
