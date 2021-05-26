a3642f81201a403f8561a8285c8ae9cffddbcc54
Ed <houedward@gmail.com>
2021-05-26 09:14:02 -0700

    Write javadoc comments for the UserSettings methods

--- app/src/main/java/com/trueproof/trueproof/utils/UserSettings.java
+++ app/src/main/java/com/trueproof/trueproof/utils/UserSettings.java
@@ -7,6 +7,7 @@ import android.util.Log;
 import androidx.annotation.Nullable;
 
 import com.amplifyframework.api.ApiException;
+import com.amplifyframework.auth.AuthException;
 import com.amplifyframework.auth.AuthUser;
 import com.amplifyframework.auth.AuthUserAttribute;
 import com.amplifyframework.core.Amplify;
@@ -44,16 +45,28 @@ public class UserSettings {
         Amplify.Auth.getCurrentUser();
     }
 
+
+    /**
+     * Gets the email for the current user.
+     * @return A string containing the email for the current user. Returns
+     * null if the user is not logged in. TODO: confirm this
+     */
     @Nullable
     public String getEmail() {
         return Amplify.Auth.getCurrentUser().getUserId();
     }
 
+    /**
+     * Gets the distillery for the current user.
+     * @param success The callback called when the distillery is successfully grabbed
+     * @param fail The callback for when an error occurs. The ApiException is passed
+     *             onto the user and the cause can be grabbed with getCause().
+     */
     public void getDistillery(Consumer<Distillery> success, Consumer<ApiException> fail) {
         AuthUser authUser = Amplify.Auth.getCurrentUser();
         if (authUser == null) fail.accept(
                 new ApiException("user is not logged in",
-                        new Exception(),
+                        new AuthException.SignedOutException(),
                         "maybe prompt the user to log in")
         );
 
@@ -78,6 +91,12 @@ public class UserSettings {
         );
     }
 
+    /**
+     * Gets the User object for the current user, which contains user settings.
+     * @param success The callback called when the User is successfully grabbed
+     * @param fail The callback for when an error occurs. The ApiException is passed
+     *             onto the user and the cause can be grabbed with getCause().
+     */
     public void getUserSettings(Consumer<User> success, Consumer<ApiException> fail) {
         AuthUser authUser = Amplify.Auth.getCurrentUser();
         if (authUser == null) fail.accept(
@@ -122,6 +141,14 @@ public class UserSettings {
         );
     }
 
+    /**
+     * Gets the value from a list of AuthUserAttribute given a key. These AuthUserAttributes are
+     * key-value pairs and contain all of the AuthUser's custom attributes as well as the built
+     * in Cognito attributes.
+     * @param attributes The list of AuthUserAttributes given by Amplify.Auth.fetchUserAttributes
+     * @param keyString The key of the AuthUserAttribute
+     * @return
+     */
     private String getValueFromAuthUserAttributesByKey(List<AuthUserAttribute> attributes, String keyString) {
         for (AuthUserAttribute attribute : attributes) {
             if (attribute.getKey().getKeyString().equals("custom:distilleryId")) {
