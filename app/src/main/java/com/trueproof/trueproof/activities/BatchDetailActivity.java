000843fb8ed3c3d7c0680c0f6721186fc2ad7930
Ed <houedward@gmail.com>
2021-05-26 10:32:27 -0700

    Add redirect to TakeMeasurement in BatchDetail

--- app/src/main/java/com/trueproof/trueproof/activities/BatchDetailActivity.java
+++ app/src/main/java/com/trueproof/trueproof/activities/BatchDetailActivity.java
@@ -1,16 +1,62 @@
 package com.trueproof.trueproof.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 
+import androidx.activity.result.contract.ActivityResultContract;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.amplifyframework.datastore.generated.model.Batch;
 import com.trueproof.trueproof.R;
+import com.trueproof.trueproof.utils.JsonConverter;
 
+import org.jetbrains.annotations.NotNull;
+
+import javax.inject.Inject;
+
+import dagger.hilt.android.AndroidEntryPoint;
+
+@AndroidEntryPoint
 public class BatchDetailActivity extends AppCompatActivity {
+    final static String REDIRECT_TO_TAKE_MEASUREMENT = "redirect_to_take_measurement";
+    final static String BATCH_JSON = "batch_json";
+    final static String TAG = "BatchDetailActivity";
+
+    @Inject
+    JsonConverter jsonConverter;
+
+    Batch batch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_batch_detail);
+
+        Intent intent = getIntent();
+
+        getBatchFromIntent(intent);
+
+        boolean redirect = intent.getBooleanExtra(REDIRECT_TO_TAKE_MEASUREMENT, false);
+        if (redirect) {
+            redirectToTakeMeasurement();
+        }
+    }
+
+    private void getBatchFromIntent(Intent intent) {
+        String json = intent.getStringExtra(BATCH_JSON);
+        if (json != null) {
+            batch = jsonConverter.batchFromJson(json);
+        } else {
+            // TODO This error state is hopefully unreachable.
+            Log.e(TAG, "No batch JSON in the intent!");
+        }
+    }
+
+    private void redirectToTakeMeasurement() {
+        Intent redirectIntent = new Intent(this, TakeMeasurementActivity.class);
+        redirectIntent.putExtra(BATCH_JSON, jsonConverter.batchToJson(batch));
+        startActivity(redirectIntent);
     }
 }
--- app/src/main/java/com/trueproof/trueproof/activities/TakeMeasurementActivity.java
+++ app/src/main/java/com/trueproof/trueproof/activities/TakeMeasurementActivity.java
@@ -45,7 +45,7 @@ public class TakeMeasurementActivity extends AppCompatActivity {
     double inputTempCorrDouble = 0.0;
     double inputProofDouble = 0.0;
     double inputProofCorrDouble = 0.0;
-  
+
     static String TAG = "t.takeMeasurement";
 
     @Inject
@@ -64,72 +64,95 @@ public class TakeMeasurementActivity extends AppCompatActivity {
 
         EditText tempField = findViewById(R.id.editTextTemperatureTakeMeasurement);
         InputFilterMinMax tempLimits = new InputFilterMinMax(1.0, 100.0);
-        tempField.setFilters(new InputFilter[]{ tempLimits });
+        tempField.setFilters(new InputFilter[]{tempLimits});
 
         EditText tempCorrectionField = findViewById(R.id.editTextTemperatureTakeMeasurement);
         InputFilterMinMax tempCorrLimits = new InputFilterMinMax(-1.0, 1.0);
-        tempCorrectionField.setFilters(new  InputFilter[]{ tempCorrLimits });
+        tempCorrectionField.setFilters(new InputFilter[]{tempCorrLimits});
 
         EditText proofField = findViewById(R.id.editTextHydrometerTakeMeasurement);
         InputFilterMinMax proofLimits = new InputFilterMinMax(1.0, 206.0);
-        proofField.setFilters(new  InputFilter[]{ proofLimits });
+        proofField.setFilters(new InputFilter[]{proofLimits});
 
         EditText proofCorrectionField = findViewById(R.id.editTextHydroCorrectionTakeMeasurement);
         InputFilterMinMax proofCorrLimits = new InputFilterMinMax(-1.0, 1.0);
-        proofCorrectionField.setFilters(new  InputFilter[]{ proofCorrLimits });
+        proofCorrectionField.setFilters(new InputFilter[]{proofCorrLimits});
 
         tempField.addTextChangedListener(new TextWatcher() {
             @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
             @Override
-            public void afterTextChanged(Editable s) {}
+            public void afterTextChanged(Editable s) {
+            }
+
             @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {calculateOnChange();}
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                calculateOnChange();
+            }
         });
 
         tempCorrectionField.addTextChangedListener(new TextWatcher() {
             @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
             @Override
-            public void afterTextChanged(Editable s) {}
+            public void afterTextChanged(Editable s) {
+            }
+
             @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {calculateOnChange();}
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                calculateOnChange();
+            }
         });
 
         proofField.addTextChangedListener(new TextWatcher() {
             @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
             @Override
-            public void afterTextChanged(Editable s) {}
+            public void afterTextChanged(Editable s) {
+            }
+
             @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {calculateOnChange();}
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                calculateOnChange();
+            }
         });
 
         proofCorrectionField.addTextChangedListener(new TextWatcher() {
             @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
             @Override
-            public void afterTextChanged(Editable s) {}
+            public void afterTextChanged(Editable s) {
+            }
+
             @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {calculateOnChange();
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                calculateOnChange();
             }
         });
 
-        }
+    }
 
-    public void calculateOnChange(){
+    public void calculateOnChange() {
         String inputTemperature = ((EditText) findViewById(R.id.editTextTemperatureMain)).getText().toString();
-        if (inputTemperature != null && inputTemperature.length() > 0 && !inputTemperature.contains(".")){
+        if (inputTemperature != null && inputTemperature.length() > 0 && !inputTemperature.contains(".")) {
             String doubleAppend = inputTemperature + ".0";
             inTempDouble = Double.parseDouble(doubleAppend);
         }
-        if (inputTemperature != null && inputTemperature.length() > 0 && inputTemperature.contains(".")){
+        if (inputTemperature != null && inputTemperature.length() > 0 && inputTemperature.contains(".")) {
             inTempDouble = Double.parseDouble(inputTemperature);
         }
         System.out.println("inTempDouble = " + inTempDouble);
         ////////////////////////
         String inputTemperatureCorrection = ((EditText) findViewById(R.id.editTextTemperatureCorrectionMain)).getText().toString();
-        if (inputTemperatureCorrection != null && inputTemperatureCorrection.length() > 0 && !inputTemperatureCorrection.contains(".")){
+        if (inputTemperatureCorrection != null && inputTemperatureCorrection.length() > 0 && !inputTemperatureCorrection.contains(".")) {
             String doubleAppend = inputTemperatureCorrection + ".0";
             inputTempCorrDouble = Double.parseDouble(doubleAppend);
         }
@@ -139,7 +162,7 @@ public class TakeMeasurementActivity extends AppCompatActivity {
         System.out.println("inputTempCorrDouble = " + inputTempCorrDouble);
         /////////////////////////
         String inputProof = ((EditText) findViewById(R.id.editTextHydrometerMain)).getText().toString();
-        if (inputProof != null && inputProof.length() > 0 && !inputProof.contains(".")){
+        if (inputProof != null && inputProof.length() > 0 && !inputProof.contains(".")) {
             String doubleAppend = inputProof + ".0";
             inputProofDouble = Double.parseDouble(doubleAppend);
         }
@@ -149,7 +172,7 @@ public class TakeMeasurementActivity extends AppCompatActivity {
         System.out.println("inputProofDouble = " + inputProofDouble);
         /////////////////////////
         String inputProofCorrection = ((EditText) findViewById(R.id.editTextHydrometerCorrectionMain)).getText().toString();
-        if (inputProofCorrection != null && inputProofCorrection.length() > 0 && !inputProofCorrection.contains(".")){
+        if (inputProofCorrection != null && inputProofCorrection.length() > 0 && !inputProofCorrection.contains(".")) {
             String doubleAppend = inputProofCorrection + ".0";
             inputProofCorrDouble = Double.parseDouble(doubleAppend);
         }
@@ -161,11 +184,9 @@ public class TakeMeasurementActivity extends AppCompatActivity {
         TextView calculatedProof = findViewById(R.id.textViewCalculatedProofMain);
 
         double proofFromProofing = proofing.proof(inTempDouble, inputProofDouble, inputProofCorrDouble, inputTempCorrDouble);
-        if (proofFromProofing < 0){
+        if (proofFromProofing < 0) {
             calculatedProof.setText("Does not exist. Check measurements and try again.");
-        }else calculatedProof.setText(String.valueOf(proofFromProofing));
+        } else calculatedProof.setText(String.valueOf(proofFromProofing));
 
     }
-    
 }
-
