a3a7ccc40dc3e4142da72c91c35e82d331c39bb3
Seamus Brown <shay_brown_13@hotmail.com>
2021-05-25 17:13:14 -0700

    add injection and make sure can take all types

--- app/src/main/java/com/trueproof/trueproof/utils/JsonConverter.java
+++ app/src/main/java/com/trueproof/trueproof/utils/JsonConverter.java
@@ -5,10 +5,28 @@ import com.amplifyframework.datastore.generated.model.Distillery;
 import com.amplifyframework.datastore.generated.model.Measurement;
 import com.google.gson.Gson;
 
-public class JsonConverter <T>{
-    public Gson gson = new Gson();
-    public String toJson (T object){
-        return gson.toJson(object);
+import javax.inject.Inject;
+import javax.inject.Singleton;
+
+@Singleton
+public class JsonConverter {
+    public Gson gson;
+
+    @Inject
+    public JsonConverter() {
+         gson = new Gson();
+    }
+
+
+
+    public String measurementToJson (Measurement measurement){
+        return gson.toJson(measurement);
+    }
+    public String batchToJson (Batch batch){
+        return gson.toJson(batch);
+    }
+    public String distilleryToJson (Distillery distillery){
+        return gson.toJson(distillery);
     }
     public Measurement measurementFromJson (String json){
          return gson.fromJson(json, Measurement.class);
