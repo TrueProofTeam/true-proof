56ece94f77633d8cc46b5b181365c00c158eb0a0
Barrett <baxance@protonmail.com>
2021-05-25 18:11:59 -0700

    calculators working, input fields limited

--- app/src/main/java/com/trueproof/trueproof/activities/TakeMeasurementActivity.java
+++ app/src/main/java/com/trueproof/trueproof/activities/TakeMeasurementActivity.java
@@ -3,14 +3,148 @@ package com.trueproof.trueproof.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.InputFilter;
+import android.text.TextWatcher;
+import android.widget.EditText;
+import android.widget.TextView;
 
+import com.amplifyframework.datastore.generated.model.Distillery;
 import com.trueproof.trueproof.R;
+import com.trueproof.trueproof.logic.InputFilterMinMax;
+import com.trueproof.trueproof.logic.Proofing;
+import com.trueproof.trueproof.utils.TestDependencyInjection;
+
+import java.io.InputStream;
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+
+import javax.inject.Inject;
 
 public class TakeMeasurementActivity extends AppCompatActivity {
+
+    static final String TAG = "TrueProof.TakeMeasurementActivity";
+
+    @Inject
+    Proofing proofing;
+
+    double inTempDouble = 0.0;
+    double inputTempCorrDouble = 0.0;
+    double inputProofDouble = 0.0;
+    double inputProofCorrDouble = 0.0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_take_measurement);
+
+        EditText tempField = findViewById(R.id.editTextTemperatureTakeMeasurement);
+        InputFilterMinMax tempLimits = new InputFilterMinMax(1.0, 100.0);
+        tempField.setFilters(new InputFilter[]{ tempLimits });
+
+        EditText tempCorrectionField = findViewById(R.id.editTextTemperatureTakeMeasurement);
+        InputFilterMinMax tempCorrLimits = new InputFilterMinMax(-1.0, 1.0);
+        tempCorrectionField.setFilters(new  InputFilter[]{ tempCorrLimits });
+
+        EditText proofField = findViewById(R.id.editTextHydrometerTakeMeasurement);
+        InputFilterMinMax proofLimits = new InputFilterMinMax(1.0, 206.0);
+        proofField.setFilters(new  InputFilter[]{ proofLimits });
+
+        EditText proofCorrectionField = findViewById(R.id.editTextHydroCorrectionTakeMeasurement);
+        InputFilterMinMax proofCorrLimits = new InputFilterMinMax(-1.0, 1.0);
+        proofCorrectionField.setFilters(new  InputFilter[]{ proofCorrLimits });
+
+        tempField.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+            @Override
+            public void afterTextChanged(Editable s) {}
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {calculateOnChange();}
+        });
+
+        tempCorrectionField.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+            @Override
+            public void afterTextChanged(Editable s) {}
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {calculateOnChange();}
+        });
+
+        proofField.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+            @Override
+            public void afterTextChanged(Editable s) {}
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {calculateOnChange();}
+        });
+
+        proofCorrectionField.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+            @Override
+            public void afterTextChanged(Editable s) {}
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {calculateOnChange();
+            }
+        });
+
+        }
+
+    public void calculateOnChange(){
+        String inputTemperature = ((EditText) findViewById(R.id.editTextTemperatureMain)).getText().toString();
+        if (inputTemperature != null && inputTemperature.length() > 0 && !inputTemperature.contains(".")){
+            String doubleAppend = inputTemperature + ".0";
+            inTempDouble = Double.parseDouble(doubleAppend);
+        }
+        if (inputTemperature != null && inputTemperature.length() > 0 && inputTemperature.contains(".")){
+            inTempDouble = Double.parseDouble(inputTemperature);
+        }
+        System.out.println("inTempDouble = " + inTempDouble);
+        ////////////////////////
+        String inputTemperatureCorrection = ((EditText) findViewById(R.id.editTextTemperatureCorrectionMain)).getText().toString();
+        if (inputTemperatureCorrection != null && inputTemperatureCorrection.length() > 0 && !inputTemperatureCorrection.contains(".")){
+            String doubleAppend = inputTemperatureCorrection + ".0";
+            inputTempCorrDouble = Double.parseDouble(doubleAppend);
+        }
+        if (inputTemperatureCorrection != null && inputTemperatureCorrection.length() > 0 && inputTemperatureCorrection.contains(".")) {
+            inputTempCorrDouble = Double.parseDouble(inputTemperatureCorrection);
+        }
+        System.out.println("inputTempCorrDouble = " + inputTempCorrDouble);
+        /////////////////////////
+        String inputProof = ((EditText) findViewById(R.id.editTextHydrometerMain)).getText().toString();
+        if (inputProof != null && inputProof.length() > 0 && !inputProof.contains(".")){
+            String doubleAppend = inputProof + ".0";
+            inputProofDouble = Double.parseDouble(doubleAppend);
+        }
+        if (inputProof != null && inputProof.length() > 0 && inputProof.contains(".")) {
+            inputProofDouble = Double.parseDouble(inputProof);
+        }
+        System.out.println("inputProofDouble = " + inputProofDouble);
+        /////////////////////////
+        String inputProofCorrection = ((EditText) findViewById(R.id.editTextHydrometerCorrectionMain)).getText().toString();
+        if (inputProofCorrection != null && inputProofCorrection.length() > 0 && !inputProofCorrection.contains(".")){
+            String doubleAppend = inputProofCorrection + ".0";
+            inputProofCorrDouble = Double.parseDouble(doubleAppend);
+        }
+        if (inputProofCorrection != null && inputProofCorrection.length() > 0 && inputProofCorrection.contains(".")) {
+            inputProofCorrDouble = Double.parseDouble(inputProofCorrection);
+        }
+        System.out.println("inputProofCorrDouble = " + inputProofCorrDouble);
+
+        TextView calculatedProof = findViewById(R.id.textViewCalculatedProofMain);
+
+        double proofFromProofing = proofing.proof(inTempDouble, inputProofDouble, inputProofCorrDouble, inputTempCorrDouble);
+        if (proofFromProofing < 0){
+            calculatedProof.setText("Does not exist. Check measurements and try again.");
+        }else calculatedProof.setText(String.valueOf(proofFromProofing));
+
     }
+    
 }
+
+
+
+
