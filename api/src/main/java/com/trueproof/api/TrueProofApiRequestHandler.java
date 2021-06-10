2a319e2784e6dff5c679d5f54afad0230342aa6f
Ed <houedward@gmail.com>
2021-06-10 16:30:41 -0700

    Made the hello world example work

--- api/src/main/java/com/trueproof/api/TrueProofApiRequestHandler.java
+++ api/src/main/java/com/trueproof/api/TrueProofApiRequestHandler.java
@@ -3,24 +3,32 @@ package com.trueproof.api;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPResponse;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Map;
 
-public class TrueProofApiRequestHandler implements RequestHandler<Map<String, String>, String> {
+public class TrueProofApiRequestHandler implements RequestHandler<APIGatewayV2HTTPEvent, APIGatewayV2HTTPResponse> {
     Gson gson = new GsonBuilder().setPrettyPrinting().create();
 
     @Override
-    public String handleRequest(Map<String, String> event, Context context) {
+    public APIGatewayV2HTTPResponse handleRequest(APIGatewayV2HTTPEvent event, Context context) {
         LambdaLogger logger = context.getLogger();
-        String response = "200 OK";
-        // log execution details
         logger.log("ENVIRONMENT VARIABLES: " + gson.toJson(System.getenv()));
         logger.log("CONTEXT: " + gson.toJson(context));
         // process event
         logger.log("EVENT: " + gson.toJson(event));
-        logger.log("EVENT TYPE: " + event.getClass().toString());
-        return response;
+
+        return APIGatewayV2HTTPResponse.builder()
+                .withBody("Hello world")
+                //.withIsBase64Encoded(false)
+                .withStatusCode(200)
+                .withHeaders(new HashMap<>())
+                .withCookies(new ArrayList<>())
+                .build();
     }
 }
